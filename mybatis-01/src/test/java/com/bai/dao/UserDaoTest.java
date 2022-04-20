55c0e031f3f0477e3f623a2662c05a98599a8de3
白子 <1373953675@qq.com>
2022-04-20 13:50:01 +0800

    P3 CRUD

--- mybatis-01/src/test/java/com/bai/dao/UserDaoTest.java
+++ mybatis-01/src/test/java/com/bai/dao/UserDaoTest.java
@@ -14,17 +14,17 @@ public class UserDaoTest {
 
         try {
             // 方式1：getMapper
-            UserDao userDao = sqlSession.getMapper(UserDao.class);
-
+            //UserDao userDao = sqlSession.getMapper(UserDao.class);
+            UserMapper userDao = sqlSession.getMapper(UserMapper.class); // 也要在核心配置文件绑定
             List<User> userList = userDao.getUserList();
             for (User user : userList) {
                 System.out.println(user);
             }
             // 方式2：不推荐
-            List<User> userList1 = sqlSession.selectList("com.bai.dao.UserDao.getUserList");
-            for (User user : userList1) {
-                System.out.println(user);
-            }
+//            List<User> userList1 = sqlSession.selectList("com.bai.dao.UserDao.getUserList");
+//            for (User user : userList1) {
+//                System.out.println(user);
+//            }
         } catch (Exception e) {
             e.printStackTrace();
 
@@ -35,4 +35,54 @@ public class UserDaoTest {
 
 
     }
+
+    @Test
+    public void getUserById() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        User user1 = userMapper.getUserById(1);
+        System.out.println(user1);
+        sqlSession.close();
+    }
+
+    @Test
+    public void addUser() {
+        // 增删改需要提交事务
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        int res = userMapper.addUser(new User(5, "哈哈", "12123"));
+        if (res > 0) {
+            System.out.println("插入成功");
+        }
+        // 提交事务
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    public void updateUser() {
+        // 增删改需要提交事务
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        int res = userMapper.updateUser(new User(2, "哈哈", "213"));
+
+
+        // 提交事务
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    public void deleteUser() {
+        // 增删改需要提交事务
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        int res = userMapper.deleteUser(4);
+
+
+        // 提交事务
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
 }
