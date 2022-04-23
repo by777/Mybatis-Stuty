cc66a38b52c2efc9af52d8a58bf704807b52d1ea
白子 <1373953675@qq.com>
2022-04-23 16:16:51 +0800

    1对多，多对1

--- mybatis-07/src/test/java/com/bai/dao/TestTS.java
+++ mybatis-07/src/test/java/com/bai/dao/TestTS.java
@@ -27,7 +27,15 @@ public class TestTS {
         SqlSession sqlSession = MybatisUtils.getSqlSession();
         TeacherMapper teacherMapper = sqlSession.getMapper(TeacherMapper.class);
         Teacher teacher = teacherMapper.getTeacher(1);
-        System.out.println();
+        Teacher teacher2 = teacherMapper.getTeacher2(1);
+//        Teacher(id=1, name=白老师,
+//        students=[Student(id=1, name=小明, tid=1),
+//        Student(id=2, name=小红, tid=1),
+//        Student(id=3, name=小张, tid=1),
+//        Student(id=4, name=小李, tid=1),
+//        Student(id=5, name=小王, tid=1)])
+        System.out.println(teacher);
+        System.out.println(teacher2);
         sqlSession.close();
     }
 
